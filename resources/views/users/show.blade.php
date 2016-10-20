d22c6f930fa5b7d3ad484f7b2a1ff6b5e6690fbb
htrevino29 <htrevino29@gmail.com>
2016-10-20 09:03:14 -0500

    blade for user posts added

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,4 +1,40 @@
 @extends('layouts.master')
+
 @section('content')
-	<h1>i was here</h1>
+	<div='container'>
+		<h1> {{ Auth::user()->name }} </h1>
+		<p> {{ Auth::user()->email }} </p>
+
+
+		<button type="submit" class="btn-success btn"><a href="{{ action('UsersController@edit', Auth::user()->id) }}">EDIT ACCOUNT</a></button>
+
+	@foreach(Auth::user()->posts as $post) 
+		<div class="posts">
+			<tr>
+				<div>
+					
+					<h4><td>{{ Auth::user()->title }}</td></h4>
+				</div>
+				<div>
+					<h4><td>{{ $post->content }}</td></h4>
+				</div>
+				<div>
+					<h4><td>{{ $post->url }}</td></h4>
+				</div>
+				<div>
+					<a href="{{ action('PostsController@show', $post->id) }}">See Post</a>
+					<p>{{ $post->created_at->diffForHumans() }}</p>
+					<button type="submit" class="btn-success btn"><a href="{{ action('PostsController@edit', $post->id) }}">EDIT POST</a></button>
+				<form method="POST" action="{{ action('PostsController@destroy', $post->id) }}">
+					{!! csrf_field() !!}
+					{!! method_field('DELETE') !!}
+					<button type="submit" class="btn-success btn">DELETE</button>
+				</form>
+				</div>
+			</tr>
+
+		</div>
+	@endforeach
+
+	</div>
 @stop
